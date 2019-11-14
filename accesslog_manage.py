f03c0849f43944d7ce3065fd011708bb629d9a4f
akhilvis <akhilviswam000@gmail.com>
2019-11-14 23:20:16 +0530

    bug fix

--- accesslog_manage.py
+++ accesslog_manage.py
@@ -2,6 +2,9 @@ import boto3
 import os, time
 import datetime
 
+# Configure AWS
+# https://boto3.amazonaws.com/v1/documentation/api/latest/guide/quickstart.html
+
 # Let's use Amazon S3
 s3 = boto3.resource('s3')
 
